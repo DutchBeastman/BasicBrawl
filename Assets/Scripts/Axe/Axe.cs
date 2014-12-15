de0090c77789224953d2e6144efe5b62022b78ac
DutchBeastman <Fabianverk@hotmail.com>
2014-12-15 21:17:13 +0100

    Update on Axe Spawning

--- Assets/Scripts/Axe/Axe.cs
+++ Assets/Scripts/Axe/Axe.cs
@@ -2,24 +2,13 @@
 using System.Collections.Generic;
 
 public class Axe : MonoBehaviour {
-	public const string AXE_PREFAB = "Prefabs/Axe";
 
-	[SerializeField]
-	private int numAxes;
-	[SerializeField]
-	private List<GameObject> axes;
 
 
 
 	// Use this for initialization
 	void Start () {
-		axes = new List<GameObject>();
 
-		for(int i = 0; i < numAxes; i++){
-
-			axes.Add(GameObject.Instantiate(Resources.Load(AXE_PREFAB),transform.position, Quaternion.identity) as GameObject);
-
-		}
 	}
 	
 	// Update is called once per frame
