1fe797db007abfec5d460a4fc02aa34f220dbd2c
DutchBeastman <Fabianverk@hotmail.com>
2014-12-18 23:40:15 +0100

    Updating scripts some more

---
+++ Assets/Scripts/Player/IPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public interface IPlayer {
+	/** <summary>Executes the player movement</summary>*/
+	 void PlayerMovement();
+
+	/** <summary>Throw the object, always moves in the forward direction</summary>
+	 * <param name="dir">The direction of the throw</param> */
+	void Throw(int direction);
+}
